5eec07d3281a90d364f7145f14dc76c377c48fc0
sherazirshad680 <134085129+sherazirshad680@users.noreply.github.com>
2024-04-23 18:48:45 +0500

    Ts 5th Assingment of Arrays

---
+++ index.js
@@ -0,0 +1,67 @@
+// var cars1: string[] = ["Crolla", "Wagonr"]
+// var cars2: string[] = ["Mehran", "Cultus"]
+// console.log(("This is Example of Concatination :"), cars1.concat(cars2));
+// var person: string[] = ["Sheraz", "Irshad", "Ahmad", "Jawad"]
+// console.log((`Example of IndexOf :`), person.indexOf(`Ahmad`));
+// var Books: string[] = ["Chemistery", "Mathemetics", "Science", "Biology"]
+// console.log(Books.join(" * "));
+// var fruits: string[] = ["Apple", "Orange", "Apple", "Mango"];
+// console.log(fruits.lastIndexOf("2"));
+// var persons: string[] = ["Sheraz", "Ahmad", "SirRizwan", "SirShoaib"]
+// console.log(("Example Of Pop ; Removed :"), persons.pop());
+// console.log(persons);
+// var fruits: string[] = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(("Example Of Push :"), fruits.push("Grapes"));
+// console.log(fruits);
+// var Myname: string[] = ["Irshad", "Sheraz"]
+// console.log(("Example Of Reverse :"), Myname.reverse());
+// var fruits: string[] = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(("Example Of Shift ; Removed:"), fruits.shift());
+// console.log(fruits);
+// var fruits: string[] = ["Banana", "Orange", "Lemon", "Apple", "Guav"];
+// console.log((`Example Of Slice :`), fruits.slice(1, 3));
+// console.log(fruits);
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(("Example Of sort :"), fruits.sort());
+// var BikesNAme: string[] = ["Honda", "Roadprince", `Jinan`, "Prider"]
+// console.log(("Example Of Splice"), BikesNAme.splice(1, 1, "Suzuki"));
+// console.log(BikesNAme);
+// var Name: string[] = ["Sheraz", "Irshad"]
+// console.log(("Example Of Tostring"), Name.toString());
+// var Animals: string[] = ["Cat", "Dog", "Cow"]
+// console.log(("Example Of Unshift :"), Animals.unshift("Rabbit", "Elephant"));
+// console.log(Animals);
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+// var num1: number[] = [1, 2]
+// var num2: number[] = [3, 4]
+// console.log((`This is Example of Concatination :`), num1.concat(num2));
+// var person: number[] = [5, 6, 7, 8]
+// console.log((`Example of IndexOf :`), person.indexOf(7));
+// var Numbers: number[] = [9, 10, 11, 12]
+// console.log(Numbers.join(" * "));
+// var nums: number[] = [13, 14, 15, 16];
+// console.log(nums.lastIndexOf(2));
+// var nmbrs: number[] = [17, 18, 19, 20]
+// console.log((`Example Of Pop ; Removed :`), nmbrs.pop());
+// console.log(nmbrs);
+// var nms: number[] = [21, 22, 23, 24];
+// console.log((`Example Of Push :`), nms.push(25));
+// console.log(nms);
+// var Hellonumber: number[] = [27, 26]
+// console.log((`Example Of Reverse :`), Hellonumber.reverse());
+// var Hinumbers: number[] = [28, 29, 30, 31];
+// console.log((`Example Of Shift ; Removed:`), Hinumbers.shift());
+// console.log(Hinumbers);
+// var num: number[] = [32, 33, 34, 35, 37];
+// console.log((`Example Of Slice :`), num.slice(1, 3));
+// console.log(num);
+// var nms = [38, 39, 40, 41];
+// console.log((`Example Of sort :`), nms.sort());
+// var NAmenms: number[] = [42, 43, 44, 45]
+// console.log((`Example Of Splice`), NAmenms.splice(1, 1, 46));
+// console.log(NAmenms);
+// var Namenms: number[] = [47, 48]
+// console.log((`Example Of Tostring`), Namenms.toString());
+// var nmms: number[] = [49, 50, 51]
+// console.log((`Example Of Unshift :`), nmms.unshift(52, 53));
+// console.log(nmms);
